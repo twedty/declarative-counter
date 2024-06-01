9a5a58acf239728dae6c75341ec40729e1ad7f2b
twedty <jamestwedt@gmail.com>
2024-05-31 22:34:01 -0400

    full counter commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Let's Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(1);
+
+    const incrementCounter = () => {
+        setCounter(counter + 1);
+    };
+
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <h2>{counter + 1}</h2>
+            <h4>{counter + 2}</h4>
+            <button onClick={incrementCounter}>Increment</button>
+        </div>
+    );
 }
 
 export default App;
